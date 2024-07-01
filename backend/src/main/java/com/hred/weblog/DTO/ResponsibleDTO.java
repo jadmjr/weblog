346ce3f1ebd4e9295a53ca097ab0405f0a98dc1c
Julimar <jadmjr@gmail.com>
2024-07-01 19:05:27 -0300

    removing unused comments

--- backend/src/main/java/com/hred/weblog/DTO/ResponsibleDTO.java
+++ backend/src/main/java/com/hred/weblog/DTO/ResponsibleDTO.java
@@ -17,7 +17,6 @@ public class ResponsibleDTO {
 	private String email;
 	private String phone;
 	private String documentNumber;
-	//colocar entidades simples
 	private ResponsibleAddress responsibleAdress;
 
 }
--- backend/src/main/java/com/hred/weblog/DTO/SenderDTO.java
+++ backend/src/main/java/com/hred/weblog/DTO/SenderDTO.java
@@ -17,7 +17,6 @@ public class SenderDTO {
 	private String email;
 	private String phone;
 	private String documentNumber;
-	//colocar entidades simples
 	private SenderAddress senderAdress;
 
 }
