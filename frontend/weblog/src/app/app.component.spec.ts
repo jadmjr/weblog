be510918c66760c57210a435b922357dc0aa4266
Julimar <jadmjr@gmail.com>
2024-07-09 18:09:48 -0300

    incluindo material

--- frontend/weblog/src/app/app.component.spec.ts
+++ frontend/weblog/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'weblog-app' title`, () => {
+  it(`should have the 'weblog' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('weblog-app');
+    expect(app.title).toEqual('weblog');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, weblog-app');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, weblog');
   });
 });
